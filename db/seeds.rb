4bdf415d8c57a0673c0a9f08d60e223488408242
tabatha wiggers <tatisabato@hotmail.com>
2022-04-30 13:28:08 -0300

    small fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,18 @@
 # Garden.destroy_all
 # puts 'DB clean!!'
 
-# Garden.create!(
-#     name: "tatchi's Garden",
-#     banner_url: 'https://images.unsplash.com/photo-1509409836982-c0d71b087fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
-# )
+Garden.create!(
+    name: "tatchi's Garden",
+    banner_url: 'https://images.unsplash.com/photo-1509409836982-c0d71b087fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+)
 
-# Garden.create!(
-#     name: "tatchi's other Garden",
-#     banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
-# )
+Garden.create!(
+    name: "tatchi's other Garden",
+    banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
 
 Plant.create!(
     name: "Rose",
-    image_url: "https://images.unsplash.com/photo-1559563362-c667ba5f5480?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=345&q=80"
+    image_url: "https://images.unsplash.com/photo-1559563362-c667ba5f5480?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=345&q=80",
     garden_id: 2
 )
